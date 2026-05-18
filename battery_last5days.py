2a4dbf08b909a53eaee81e6227e5514519665e43
DCpiZ <DCpiZ>
2026-05-18 15:26:58 +0100

    Adding query to pull last 5 days of battery readings for each sensor

---
+++ battery_last5days.py
@@ -0,0 +1,9 @@
+import sqlite3
+con = sqlite3.connect('readings.db')
+for r in con.execute('''
+    SELECT datetime(timestamp, \"localtime\"), sensor, room, reading_type, value
+    FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY sensor ORDER BY timestamp ASC) AS rn FROM readings WHERE reading_type = \"battery\")
+    WHERE rn <= 5
+    ORDER BY room, timestamp ASC
+'''): print(r)
+
