df05c578ccd951c28999cc9a61e7064def44f153
DCpiZ <DCpiZ>
2026-05-18 15:29:45 +0100

    ASC --> DESC in battery_last5days

--- battery_last5days.py
+++ battery_last5days.py
@@ -2,7 +2,7 @@ import sqlite3
 con = sqlite3.connect('readings.db')
 for r in con.execute('''
     SELECT datetime(timestamp, \"localtime\"), sensor, room, reading_type, value
-    FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY sensor ORDER BY timestamp ASC) AS rn FROM readings WHERE reading_type = \"battery\")
+    FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY sensor ORDER BY timestamp DESC) AS rn FROM readings WHERE reading_type = \"battery\")
     WHERE rn <= 5
     ORDER BY room, timestamp ASC
 '''): print(r)
