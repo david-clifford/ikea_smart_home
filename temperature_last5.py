609c6b540bea4f134d9c2683a332d26fc0622b79
DCpiZ <DCpiZ>
2026-05-28 07:56:21 +0100

    Add temperature query for last 5 readings

---
+++ temperature_last5.py
@@ -0,0 +1,9 @@
+import sqlite3
+con = sqlite3.connect('readings.db')
+for r in con.execute('''
+    SELECT datetime(timestamp, \"localtime\"), sensor, room, reading_type, value
+    FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY sensor ORDER BY timestamp DESC) AS rn FROM readings WHERE reading_type = \"temperature\")
+    WHERE rn <= 5
+    ORDER BY room, timestamp ASC
+'''): print(r)
+
